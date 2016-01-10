f19d05244bae6e9380b13f2763f4f2d34435fd86
Jack Schuss <jackschuss@gmail.com>
2016-01-09 20:54:08 -0500

    change views, add username migration, add username to registrations#new view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+    devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:username, :email, :password)}
+    devise_parameter_sanitizer.for(:account_update) {|u| u.permit(:username, :email, :password, :current_password)}
   end
 end
